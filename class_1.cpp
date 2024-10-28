82a21c1f61cb0d7667d69a02ef1f6895b8d9ab1b
UMAKANT06 <sahuumakant877@gmail.com>
2024-10-28 09:07:33 +0530

    done

--- class_1.cpp
+++ class_1.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Player{
+    public:
+        int x,y;
+        int speed;
+};
 int main(){
-    
+    Player uma;
+    cout<<uma.speed;
+
+    return 0;
 }
